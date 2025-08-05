c987a5b87b030a11b7ebd36ec707e99d88704cf3
kevinhehc <143791417+kevinhehc@users.noreply.github.com>
2025-08-05 22:22:51 +0800

    lb --- 负载均衡算法

--- zrpc/internal/balancer/p2c/p2c.go
+++ zrpc/internal/balancer/p2c/p2c.go
@@ -62,7 +62,7 @@ func (b *p2cPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 	}
 
 	return &p2cPicker{
-		conns: conns,
+		conns: conns, // 保存所有节点的信息
 		r:     rand.New(rand.NewSource(time.Now().UnixNano())),
 		stamp: syncx.NewAtomicDuration(),
 	}
@@ -85,15 +85,15 @@ func (p *p2cPicker) Pick(_ balancer.PickInfo) (balancer.PickResult, error) {
 
 	var chosen *subConn
 	switch len(p.conns) {
-	case 0:
+	case 0: // 没有节点，返回错误
 		return emptyPickResult, balancer.ErrNoSubConnAvailable
-	case 1:
+	case 1: // 有一个节点，直接返回这个节点
 		chosen = p.choose(p.conns[0], nil)
-	case 2:
+	case 2: // 有两个节点，计算负载，返回负载低的节点
 		chosen = p.choose(p.conns[0], p.conns[1])
-	default:
+	default: // 有多个节点，p2c 挑选两个节点，比较这两个节点的负载，返回负载低的节点
 		var node1, node2 *subConn
-		for i := 0; i < pickTimes; i++ {
+		for i := 0; i < pickTimes; i++ { // 3次随机选择两个节点
 			a := p.r.Intn(len(p.conns))
 			b := p.r.Intn(len(p.conns) - 1)
 			if b >= a {
@@ -101,11 +101,12 @@ func (p *p2cPicker) Pick(_ balancer.PickInfo) (balancer.PickResult, error) {
 			}
 			node1 = p.conns[a]
 			node2 = p.conns[b]
+			// 如果这次选择的节点达到了健康要求, 就中断选择
 			if node1.healthy() && node2.healthy() {
 				break
 			}
 		}
-
+		// 比较两个节点的负载情况，选择负载低的
 		chosen = p.choose(node1, node2)
 	}
 
@@ -121,8 +122,10 @@ func (p *p2cPicker) Pick(_ balancer.PickInfo) (balancer.PickResult, error) {
 func (p *p2cPicker) buildDoneFunc(c *subConn) func(info balancer.DoneInfo) {
 	start := int64(timex.Now())
 	return func(info balancer.DoneInfo) {
+		// 正在处理的请求数减 1
 		atomic.AddInt64(&c.inflight, -1)
 		now := timex.Now()
+		// 保存本次请求结束时的时间点，并取出上次请求时的时间点
 		last := atomic.SwapInt64(&c.last, int64(now))
 		td := int64(now) - last
 		if td < 0 {
@@ -133,7 +136,9 @@ func (p *p2cPicker) buildDoneFunc(c *subConn) func(info balancer.DoneInfo) {
 		// the value of w (y-axis) will decrease, inversely proportional.
 		// The function curve of y = x^(-x) is as follows:
 		// https://github.com/zeromicro/zero-doc/blob/main/doc/images/y_e_x.png?raw=true
+		// 用牛顿冷却定律中的衰减函数模型计算EWMA算法中的β值
 		w := math.Exp(float64(-td) / float64(decayTime))
+		// 保存本次请求的耗时
 		lag := int64(now) - start
 		if lag < 0 {
 			lag = 0
@@ -144,6 +149,7 @@ func (p *p2cPicker) buildDoneFunc(c *subConn) func(info balancer.DoneInfo) {
 		}
 
 		// The smaller the value of w, the lower the impact of historical data.
+		// 计算 EWMA 值
 		atomic.StoreUint64(&c.lag, uint64(float64(olag)*w+float64(lag)*(1-w)))
 		success := initSuccess
 		if info.Err != nil && !codes.Acceptable(info.Err) {
@@ -161,6 +167,8 @@ func (p *p2cPicker) buildDoneFunc(c *subConn) func(info balancer.DoneInfo) {
 	}
 }
 
+// 1. 如果超过 forcePick = 1s 没有选中过了，会强制选中
+// 2. 如果没有超过 forcePick 的时间，那就根据负载来选中
 func (p *p2cPicker) choose(c1, c2 *subConn) *subConn {
 	start := int64(timex.Now())
 	if c2 == nil {
@@ -197,15 +205,15 @@ func (p *p2cPicker) logStats() {
 
 type subConn struct {
 	// The request latency measured by the weighted moving average algorithm.
-	lag uint64
+	lag uint64 // 用来保存 ewma 值
 
 	// The value represents the number of requests that are either pending or just
 	// starting at the current node, and it is obtained through atomic addition.
-	inflight int64
-	success  uint64
-	requests int64
-	last     int64
-	pick     int64
+	inflight int64  // 用在保存当前节点正在处理的请求总数
+	success  uint64 // 用来标识一段时间内此连接的健康状态
+	requests int64  // 用来保存请求总数
+	last     int64  // 用来保存上一次请求耗时, 用于计算 ewma 值
+	pick     int64  // 保存上一次被选中的时间点
 	addr     resolver.Address
 	conn     balancer.SubConn
 }
@@ -216,6 +224,7 @@ func (c *subConn) healthy() bool {
 
 func (c *subConn) load() int64 {
 	// plus one to avoid multiply zero
+	// 通过 EWMA 计算节点的负载情况； 加 1 是为了避免为 0 的情况
 	lag := int64(math.Sqrt(float64(atomic.LoadUint64(&c.lag) + 1)))
 	load := lag * (atomic.LoadInt64(&c.inflight) + 1)
 	if load == 0 {
