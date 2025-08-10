418a7f2355399a36831da566efc7ec81c7b885ae
kevinhehc <143791417+kevinhehc@users.noreply.github.com>
2025-08-10 15:21:02 +0800

    熔断器的使用

--- zrpc/internal/clientinterceptors/breakerinterceptor.go
+++ zrpc/internal/clientinterceptors/breakerinterceptor.go
@@ -12,8 +12,12 @@ import (
 // BreakerInterceptor is an interceptor that acts as a circuit breaker.
 func BreakerInterceptor(ctx context.Context, method string, req, reply any,
 	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// 根据方法名获取或创建一个熔断器
 	breakerName := path.Join(cc.Target(), method)
+	// 执行熔断逻辑
 	return breaker.DoWithAcceptableCtx(ctx, breakerName, func() error {
+		// 如果熔断器允许请求通过，则调用真正的 gRPC invoker
 		return invoker(ctx, method, req, reply, cc, opts...)
+		// isAcceptable 是一个函数，用来判断哪些错误可以被熔断器忽略
 	}, codes.Acceptable)
 }
